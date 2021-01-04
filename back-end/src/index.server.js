b57efd92b4595840143d968b6b6d468ccaa96f5c
asifali2705 <asifalieas@gmail.com>
2021-01-04 23:02:10 +0530

    initial commit, startedd authentication API

---
+++ back-end/src/index.server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const env = require("dotenv");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+//routes
+const userRoutes = require("./routes/user");
+
+env.config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.jx5qf.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log("database connected");
+  });
+
+app.use(bodyParser());
+app.use("/api", userRoutes);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
