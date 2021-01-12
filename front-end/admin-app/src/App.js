317fd68ea86735b2b7b78834a9b175feaf873f6e
asifali2705 <asifalieas@gmail.com>
2021-01-12 21:30:36 +0530

    started admin-dashboard

---
+++ front-end/admin-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <div className="App">
+      <Layout />
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
