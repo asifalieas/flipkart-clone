0094bd333fb7b5725e4713dec9a6732f73ff1f1d
asifali2705 <asifalieas@gmail.com>
2021-04-16 12:43:28 +0530

    category and subcategory API added

--- back-end/src/index.server.js
+++ back-end/src/index.server.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 //routes
 const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin/auth");
+const categoryRoutes = require("./routes/category");
 
 env.config();
 
@@ -25,6 +26,7 @@ mongoose
 app.use(express.json());
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
+app.use("/api", categoryRoutes);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
