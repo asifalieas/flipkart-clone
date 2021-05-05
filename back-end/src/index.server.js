3532ab3afe85d5f65d8931a2c82bc4ac088bf49e
Asif Ali <asifalieas@gmail.com>
2021-05-05 13:34:12 +0530

    Admin Login and Private Route setup

--- back-end/src/index.server.js
+++ back-end/src/index.server.js
@@ -3,6 +3,7 @@ const app = express();
 const env = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
+const cors = require("cors");
 
 //routes
 const authRoutes = require("./routes/auth");
@@ -27,6 +28,7 @@ mongoose
     console.log("database connected");
   });
 
+app.use(cors());
 app.use(express.json());
 app.use("/public", static(path.join(__dirname, "uploads")));
 app.use("/api", authRoutes);
